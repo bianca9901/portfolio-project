64213c1c60070a1747cb07ab693d461f1c727bf5
bianca9901 <biancawikberg99@yahoo.com>
2024-02-05 13:35:52 +0100

    Add onClick to Intro component for direct navigation to Contact

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -8,9 +8,11 @@ import { BsArrowRight } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Intro() {
   const { ref } = useSectionInView('Home', 0.5)
+  const { setActiveSection, setTimeOfLastClick} = useActiveSectionContext()
   
   return (
     <section ref={ref} id="home" className="mb-28 max-w-[50rem] text-center sm:mb-0
@@ -77,6 +79,10 @@ export default function Intro() {
         py-3 flex items-center gap-2 rounded-full outline-none
         focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105
         transition "
+        onClick={() => {
+          setActiveSection("Contact")
+          setTimeOfLastClick(Date.now())
+        }}
         >
           Contact me here <BsArrowRight className="opacity-70 
           group-hover:translate-x-1 transition"/>
