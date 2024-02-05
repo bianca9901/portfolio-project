0905d6feee892c91069ec7818a808b94062246ea
bianca9901 <biancawikberg99@yahoo.com>
2024-02-05 09:55:11 +0100

    Implement useActiveSectionContext hook for section tracking

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+"use client"
+
+import React, { useEffect } from "react";
 import SectionHeading from "./SectionHeading";
 import { projectsData } from "@/lib/data";
 import Project from "./Project";
+import { useActiveSectionContext } from "@/context/active-section-context";
+import { useInView } from "react-intersection-observer";
 
 export default function Projects() {
+  const { ref, inView } = useInView();
+  const { setActiveSection } = useActiveSectionContext();
+
+  useEffect(() => {
+    if (inView) {
+      setActiveSection("Projects");
+    }
+  }, [inView, setActiveSection]);
+
   return (
-    <section id="projects" className="scroll-mt-28">
+    <section ref={ref} id="projects" className="scroll-mt-28">
       <SectionHeading>My projects</SectionHeading>
       <div>
         {projectsData.map((project, index) => (
