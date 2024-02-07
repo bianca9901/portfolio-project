b959a471c1d52a6ac1b4ead603ce8f40bb94aec1
bianca9901 <biancawikberg99@yahoo.com>
2024-02-07 09:36:45 +0100

    Add managing of 'dark' class on document element based on theme state

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -12,9 +12,11 @@ export default function ThemeSwitch() {
         if (theme === 'light') {
             setTheme('dark')
             window.localStorage.setItem("theme", "dark")
+            document.documentElement.classList.add("dark")
         } else {
             setTheme('light')
             window.localStorage.setItem("theme", "light")
+            document.documentElement.classList.remove("light")
         }
     }
 
@@ -22,8 +24,13 @@ export default function ThemeSwitch() {
         const localTheme = window.localStorage.getItem('theme') as Theme | null
         if (localTheme) {
             setTheme(localTheme)
+
+            if(localTheme === "dark") {
+                document.documentElement.classList.add("dark")
+            }
         } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
             setTheme("dark")
+            document.documentElement.classList.add("dark")
         }
     }, [])
 
@@ -32,7 +39,7 @@ export default function ThemeSwitch() {
     <button className='fixed bottom-5 right-5 bg-white w-[3rem] h-[3rem]
     bg-opacity-80 backdrop-blur-[0.5rem] border border-white
     border-opacity-40 shadow-2xl rounded-full flex items-center
-    justify-center hover:scale-[1.15] active:scale-105 transition-all'
+    justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950'
     onClick={toggleTheme}>
         {
             theme === "light" ? (
