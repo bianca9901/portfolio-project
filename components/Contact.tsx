15d8f8a82c657d86f53c57ed1292b65827782d37
bianca9901 <biancawikberg99@yahoo.com>
2024-02-07 14:44:43 +0100

    Fix spacing misstake

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -33,9 +33,9 @@ export default function Contact() {
       <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
         <a className="underline" href="mailto:biancawikberg99@yahoo.com">
-          biancawikberg99@yahoo.com{" "}
+          biancawikberg99@yahoo.com
         </a>
-        or through this form
+        {" "}or through this form
       </p>
 
       <form
