ee8e3dfed92b704f8b5b365e9801b43e87d496c3
bianca9901 <biancawikberg99@yahoo.com>
2024-02-07 09:56:19 +0100

    Refactor ThemeSwitch component and introduce theme-context

---
+++ context/theme-context.tsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState, createContext } from "react";
+
+type Theme = "light" | "dark";
+
+type ThemeContextProviderProps = {
+  children: React.ReactNode;
+};
+
+type ThemContextType = {
+  theme: Theme;
+  toggleTheme: () => void;
+};
+
+const ThemeContext = createContext<ThemContextType | null>(null);
+
+export default function ThemeContextProvider({ children }: ThemeContextProviderProps) {
+  const [theme, setTheme] = useState<Theme>("light");
+
+  const toggleTheme = () => {
+    if (theme === "light") {
+      setTheme("dark");
+      window.localStorage.setItem("theme", "dark");
+      document.documentElement.classList.add("dark");
+    } else {
+      setTheme("light");
+      window.localStorage.setItem("theme", "light");
+      document.documentElement.classList.remove("light");
+    }
+  };
+
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem("theme") as Theme | null;
+    if (localTheme) {
+      setTheme(localTheme);
+
+      if (localTheme === "dark") {
+        document.documentElement.classList.add("dark");
+      }
+    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      setTheme("dark");
+      document.documentElement.classList.add("dark");
+    }
+  }, []);
+
+  return (
+    <ThemeContext.Provider
+      value={{
+        theme,
+        toggleTheme,
+      }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
+}
