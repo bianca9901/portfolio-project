ab2d2f3dd4ce4b64e45285323a8639175c03fac0
bianca9901 <biancawikberg99@yahoo.com>
2024-02-07 12:43:14 +0100

    Add responsive styling to Project section

--- components/Project.tsx
+++ components/Project.tsx
@@ -33,12 +33,12 @@ export default function Project({
       <section
         className=" bg-gray-100 max-w-[42rem] border border-black/5
         rounded-lg overflow-hidden sm:pr-8 relative sm:h-[20rem]
-     hover:bg-gray-200 transition group-even:even:pl-8 dark:bg-white/10
+     hover:bg-gray-200 transition sm:group-even:even:pl-8 dark:bg-white/10
      dark:hover:bg-white/20 dark:text-white"
       >
         <div
           className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%]
-      flex flex-col h-full group-even:ml-[18rem]"
+      flex flex-col h-full sm:group-even:ml-[18rem]"
         >
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700
@@ -62,6 +62,7 @@ export default function Project({
           quality={95}
           className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg
         shadow-2xl
+        hidden sm:block
         group-even:-right-[initial]
         group-even:-left-40
         group-hover:-translate-x-3
